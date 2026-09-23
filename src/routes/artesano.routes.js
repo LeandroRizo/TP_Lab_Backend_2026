a0bc413244c4b4f1bfa058310f37a99740d720bd
Maximiliano Braga <maximilianobraga22@gmail.com>
2026-09-22 23:00:30 -0300

    feat: agregar validacion de artesanos

--- src/routes/artesano.routes.js
+++ src/routes/artesano.routes.js
@@ -8,12 +8,14 @@ import {
     eliminar
 } from "../controllers/artesano.controller.js";
 
+import { validarArtesano } from "../middlewares/validarArtesano.js";
+
 const router = express.Router();
 
 router.get("/", obtenerArtesanos);
-router.post("/", crear);
+router.post("/", validarArtesano, crear);
 router.get("/:id", obtenerPorId);
-router.put("/:id", actualizar);
+router.put("/:id", validarArtesano, actualizar);
 router.delete("/:id", eliminar);
 
 export default router;
